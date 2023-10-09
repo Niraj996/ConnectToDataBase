6124d5772e4500be0f2d8f78a81903500302634b
Niraj Baral <140519375+Niraj996@users.noreply.github.com>
2023-10-09 23:04:50 +0545

    DataBaseconnection

--- App.java
+++ App.java
@@ -1,41 +1,36 @@
-package practice.DataBase.prac.src.main.java.com.example;
+package com.example;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
 
-public  class Connect {
-    public static void main(String[] args) throws SQLException {
+public class App {
+    public static void main(String[] args) {
+        connection();
+    }
+    public static void connection() {
+
         try {
-            // Create Connection with DataBase with th help of Driver:
             Class.forName("com.mysql.cj.jdbc.Driver");
-            String url = "jdbc:mysql://localhost:3306/base";
+            //System.out.println("Driver Found"); just to check driver availability
+            
+            String url = "jdbc:mysql://localhost:3306/mybase";
             String user = "root";
             String pass = "";
             Connection conn = DriverManager.getConnection(url, user, pass);
-
-                // need sql statement to perform a query on database.
-            String sql ="select*from tbl_base";
-
-                // create a statement and connect(sends) to the database. thus conn.create statement
-            Statement stmt = conn.createStatement();
-            
-                ResultSet rs = stmt.executeQuery(sql);
-
-                while(rs.next()){
-                    int id = rs.getInt("id");
-                    String name =rs.getString("name");
-
-                    System.out.printf("%s \n%s " , id, name);
-                }
-           // System.out.println("Driver Found");
-        } catch (ClassNotFoundException ce) {
-            System.out.println("Driver not Found");
-            System.out.println(ce.getMessage());     
-
+            String creatDb = "Create database firstDB";
+            PreparedStatement stmat = conn.prepareStatement(creatDb);
+            ResultSet rs = stmat.executeQuery();
+            while (rs.next()) {
+                System.out.println("ID: " + rs.getInt("ID"));
+                System.out.println("name: " + rs.getString("name"));
+                System.out.println("Contact: " + rs.getString("cont"));
+                System.out.println("Address: " + rs.getString("Address"));
+            }
+        } catch (Exception e) {
+            e.getMessage();
+            System.out.println("Driver Not Found");
         }
+    }
 }
-}
-             
